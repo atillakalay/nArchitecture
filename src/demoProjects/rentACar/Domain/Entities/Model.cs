bc79ae1b906830137b700d29c635f176d25145bd
atilla kalay <atilla.kalayy@gmail.com>
2022-09-16 23:51:20 +0300

    4. gün ilk bölüm tamamlandı.

---
+++ src/demoProjects/rentACar/Domain/Entities/Model.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+    public class Model : Entity
+    {
+        public int BrandId { get; set; }
+        public string? Name { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string? ImageUrl { get; set; }
+        public Brand? Brand { get; set; }
+        public Model()
+        {
+
+        }
+        public Model(int id, string name, int brandId, string? imageUrl, decimal dailyPrice) : this()
+        {
+            Id = id;
+            BrandId = brandId;
+            Name = name;
+            ImageUrl = imageUrl;
+            DailyPrice = dailyPrice;
+        }
+    }
+}
--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -8,6 +8,7 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<Model> Models { get; set; }
 
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
@@ -29,6 +30,19 @@ namespace Persistence.Contexts
                 a.ToTable("Brands").HasKey(k => k.Id);
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+                a.HasMany(p => p.Models);
+            });
+
+            modelBuilder.Entity<Model>(m =>
+            {
+                m.ToTable("Models").HasKey(k => k.Id);
+                m.Property(p => p.Id).HasColumnName("Id");
+                m.Property(p => p.BrandId).HasColumnName("BrandId");
+                m.Property(p => p.Name).HasColumnName("Name");
+                m.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
+                m.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
+                m.HasOne(p => p.Brand);
+
             });
 
 
@@ -36,6 +50,8 @@ namespace Persistence.Contexts
             Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
             modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
 
+            Model[] modelEntitySeedsModels = { new Model(id: 1, name: "Series 4", brandId: 1, dailyPrice: 1500, imageUrl: ""), new Model(id: 2, name: "A 180", brandId: 2, dailyPrice: 1000, imageUrl: ""), new Model(id: 3, name: "Series 3", brandId: 1, dailyPrice: 1200, imageUrl: "") };
+            modelBuilder.Entity<Model>().HasData(modelEntitySeedsModels);
 
         }
     }
